ba1509dcb85ab24904571cfa718ec3c0b7918214
vamshikumar7735300462 <154436703+vamshikumar7735300462@users.noreply.github.com>
2025-07-21 10:50:59 +0530

    Delete chunk.py

--- chunk.py
+++
@@ -1,7 +0,0 @@
-from nltk.tokenize import word_tokenize
-from nltk import pos_tag,ne_chunk
-txt="break the sysetem dowan is the procees .the include "
-t=ne_chunk(pos_tag(word_tokenize(txt)))
-print(t)
-# import nltk
-# nltk.download('maxent_ne_chunker_tab')
