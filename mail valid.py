21d3ab985d7e54ceaab3a91119532d26b2c5789a
vamshikumar7735300462 <154436703+vamshikumar7735300462@users.noreply.github.com>
2025-07-25 09:41:29 +0530

    Delete mail valid.py

--- mail valid.py
+++
@@ -1,6 +0,0 @@
-import re 
-input=input("enter a valid mail id:")
-if re.match("^[a-z]+[\._]?+[a-z]+[@]\w+[.]\w{2,3}$",input):
-    print("valid mail id",input)
-else:
-    print("invalid mail id",input)
